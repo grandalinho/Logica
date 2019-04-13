3d331fef8146eb1952f0b3e1c6412ed1ea59742f
Jonas Nicioli <34448979+grandalinho@users.noreply.github.com>
2019-04-12 21:38:32 -0300

    Update churrasco.js
    up1

--- churrasco/churrasco.js
+++ churrasco/churrasco.js
@@ -9,16 +9,26 @@
  let totalChurrasco
 
  function calcularChurrasco() {
-     let homem, mulher, crianca, carne, refrigerante, cerveja
+
+    let homem, mulher, crianca, carne, refrigerante, cerveja, totalHomem, totalMulher, totalCrianca, totalCada
+
      homem = parseFloat(frmChurrasco.txtHomem.value.replace(",","."))
      mulher = parseFloat(frmChurrasco.txtMulher.value.replace(",","."))
-     Crianca = parseFloat(frmChurrasco.txtCrianca.value.replace(",","."))
+     crianca = parseFloat(frmChurrasco.txtCrianca.value.replace(",","."))
 
      carne = parseFloat(frmChurrasco.txtCarne.value.replace(",","."))
      refrigerante = parseFloat(frmChurrasco.txtRefrigerante.value.replace(",","."))
      cerveja = parseFloat(frmChurrasco.txtCerveja.value.replace(",","."))
 
-    totalChurrasco = ((12 * cerveja) * homem + (carne * 0.3 ) * homem + (4 * cerveja) * mulher + (2 * refrigerante) * mulher + (carne * 0.3) * mulher + (refrigerante * 2) * crianca + (0.1 * carne) * crianca)
 
-     frmChurrasco.txtTotal.value = totalChurrasco.toFixed(0)
+    totalHomem = (12 * cerveja) * homem + (carne * 0.3 ) * homem 
+    totalMulher = (4 * cerveja) * mulher + (2 * refrigerante) * mulher + (carne * 0.3) * mulher
+    totalCrianca = (refrigerante * 2) * crianca + (0.1 * carne) * crianca
+    totalChurrasco = totalHomem + totalMulher + totalCrianca
+
+    
+    totalCada = totalChurrasco / (mulher + homem)
+    
+     frmChurrasco.txtTotal.value = totalChurrasco.toFixed(2)
+     frmChurrasco.txtCada.value = totalCada.toFixed(2)
  }
